da884b32d3199d7e21be2dbb258af34f5506150d
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2023-06-02 11:42:11 -0400

    Update 1: Starter Class Created

--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -1,8 +1,5 @@
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-
 public class CalculatorRunner {
     public static void main(String[] args) {
-        MainGUI calculator = new MainGUI()
+        MainGUI calculator = new MainGUI();
     }
 }
